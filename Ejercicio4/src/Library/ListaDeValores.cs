4d23ed0db7faff1f79e153701cba6bd602bbf1fc
Felipe <felipe.cabrera@correo.ucu.edu.uy>
2020-09-08 19:47:14 -0300

    ejercicio4

---
+++ Ejercicio4/src/Library/ListaDeValores.cs
@@ -0,0 +1,23 @@
+using System;
+using System.IO;
+
+namespace Library
+{
+    public class ListaDeValores
+    {
+        string url = C:\Users\Administrador\Documents\programacion2\ej\Ejercicio4\matriz.txt;
+string content = File.ReadAllText(url);
+string[] contentLines = content.Split('\n');
+bool[,] board = new bool[contentLines.Length, contentLines[0].Length];
+for (int  y=0; y<contentLines.Length;y++)
+{
+    for (int x=0; x<contentLines[y].Length; x++)
+    {
+        if(contentLines[y][x]=='1')
+        {
+            board[x,y]=true;
+        }
+    }
+}
+    }
+}
